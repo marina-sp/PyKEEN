e4a137f2f4f51032c789819dacdd48c91e2d0fe1
marina-sp <aquamarin1995@gmail.com>
2019-04-24 03:35:03 +0700

    add relation radius embedding and remove scoring function from hyperlist

--- src/pykeen/kge_models/region.py
+++ src/pykeen/kge_models/region.py
@@ -26,7 +26,6 @@ log = logging.getLogger(__name__)
 @dataclass
 class RegionConfig:
     lp_norm: str
-    scoring_function_norm: str
     radius_init: float
     reg_lambda: float
 
@@ -57,7 +56,6 @@ class Region(BaseModule):
     model_name = REGION_NAME
     margin_ranking_loss_size_average: bool = True
     hyper_params = BaseModule.hyper_params + [
-        SCORING_FUNCTION_NORM,
         NORM_FOR_NORMALIZATION_OF_ENTITIES,
         RADIUS_INITIAL_VALUE
     ]
@@ -68,10 +66,10 @@ class Region(BaseModule):
 
         # Embeddings
         self.l_p_norm_entities = config.lp_norm
-        self.scoring_fct_norm = config.scoring_function_norm
         self.relation_embeddings = nn.Embedding(self.num_relations, self.embedding_dim)
-        self.radius = nn.Parameter(torch.FloatTensor([config.radius_init]), requires_grad=True)
+        self.relation_regions = nn.Embedding(self.num_relations, 1)
         self.reg_l = config.reg_lambda
+        self.init_radius = config.radius_init
 
         # TODO: add config parameter and move to base class
         self.criterion = nn.MarginRankingLoss(
@@ -82,7 +80,6 @@ class Region(BaseModule):
         # Output type (used for scoring)
         self.prob_mode = True
 
-
         self._initialize()
 
     def _initialize(self):
@@ -92,12 +89,21 @@ class Region(BaseModule):
             a=-embeddings_init_bound,
             b=+embeddings_init_bound,
         )
+
+        # todo: relation initialization
         nn.init.uniform_(
             self.relation_embeddings.weight.data,
             a=-embeddings_init_bound,
             b=+embeddings_init_bound,
         )
 
+        # todo: relation initialization
+        nn.init.constant_(
+            self.relation_regions.weight.data,
+            self.init_radius
+        )
+
+        # todo: relation normalization at init
         norms = torch.norm(self.relation_embeddings.weight, p=2, dim=1).data
         self.relation_embeddings.weight.data = self.relation_embeddings.weight.data.div(
             norms.view(self.num_relations, 1).expand_as(self.relation_embeddings.weight))
@@ -127,32 +133,47 @@ class Region(BaseModule):
         negative_scores = torch.tensor(negative_scores, dtype=torch.float, device=self.device)
 
         loss = self.criterion(positive_scores, - negative_scores, y)
-        # TODO: add regularization via loss = loss.add()
-        loss_ = self.reg_l * loss.add(torch.pow(torch.norm(self.radius, 2), 2))
+        #print("loss: %0.2f"%loss)
+        # TODO: regularization once or for every element
+        # todo: if here -- then once a batch -- normalized to batch size?
+        loss = loss.add(
+            self.reg_l *
+            torch.pow(
+                torch.norm(self.relation_regions.weight.data.view(-1), 2),
+                2))
+        #print("reg loss: %0.2f"%loss)
         #print(positive_scores, negative_scores, loss, loss_)
-        return loss_
+        return loss
 
     def _score_triples(self, triples):
-        head_embeddings, relation_embeddings, tail_embeddings = self._get_triple_embeddings(triples)
-        scores = self._compute_scores(head_embeddings, relation_embeddings, tail_embeddings)
+        head_embeddings, relation_embeddings, tail_embeddings, relation_regions = \
+            self._get_triple_embeddings(triples)
+        scores = self._compute_scores(
+            head_embeddings,
+            relation_embeddings,
+            tail_embeddings,
+            relation_regions)
         return scores
 
-    def _compute_scores(self, head_embeddings, relation_embeddings, tail_embeddings):
+    def _compute_scores(self, head_embeddings, relation_embeddings, tail_embeddings, regions):
         """Compute the scores based on the head, relation, and tail embeddings.
 
         :param head_embeddings: embeddings of head entities of dimension batchsize x embedding_dim
         :param relation_embeddings: embeddings of relation embeddings of dimension batchsize x embedding_dim
         :param tail_embeddings: embeddings of tail entities of dimension batchsize x embedding_dim
+        :param regions: relation region descriptor of dimension batchsize x 1
         :return: Tensor of dimension batch_size containing the scores for each batch element
         """
-        m_x   = (head_embeddings + relation_embeddings - tail_embeddings).unsqueeze(-1)
-        sigma = torch.log(1 + torch.exp(self.radius))
-        A     = sigma * torch.eye(self.embedding_dim, device=self.device)
-        dists = torch.matmul(
-            torch.matmul(m_x.transpose(-1, -2), A),
+        m_x    = (head_embeddings + relation_embeddings - tail_embeddings).unsqueeze(-1)
+        sigmas = torch.log(1 + torch.exp(regions)).unsqueeze(-1).unsqueeze(-1)
+        region_m = (sigmas * torch.eye(self.embedding_dim, device=self.device)).squeeze(1)
+        dists  = torch.matmul(
+            torch.matmul(m_x.transpose(-1, -2), region_m),
             m_x).squeeze(-1)
-        # TODO: try other activation, like tanh istead of sigmoid
-        probs = 1 / (1 + dists)
+        # TODO: try other activation, like tanh instead of sigmoid
+        if (dists == 0).any():
+            print("zero distances in loss computation")
+        probs = 1.0 / (1 + dists)
         #print("Shapes", m_x.shape, sigma, A.shape, dists.shape, probs.shape)
         #print("Dist values: ", dists[:10])
         #print("Probs values: ", probs[:10])
@@ -165,7 +186,11 @@ class Region(BaseModule):
             self._get_entity_embeddings(heads),
             self._get_relation_embeddings(relations),
             self._get_entity_embeddings(tails),
+            self._get_relation_regions(relations)
         )
 
     def _get_relation_embeddings(self, relations):
         return self.relation_embeddings(relations).view(-1, self.embedding_dim)
+
+    def _get_relation_regions(self, relations):
+        return self.relation_regions(relations).view(-1, 1)
