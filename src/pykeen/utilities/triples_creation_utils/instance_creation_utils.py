9f0a4997b69ce91afc5bb7889ff0484f3f04cb56
marina-sp <aquamarin1995@gmail.com>
2019-05-14 11:27:26 +0200

    integrate relation types into training data

--- src/pykeen/utilities/triples_creation_utils/instance_creation_utils.py
+++ src/pykeen/utilities/triples_creation_utils/instance_creation_utils.py
@@ -17,7 +17,7 @@ def create_mapped_triples(
         triples: np.ndarray,
         entity_label_to_id: Optional[Dict[str, int]] = None,
         relation_label_to_id: Optional[Dict[str, int]] = None,
-) -> Tuple[np.ndarray, Dict[str, int], Dict[str, int]]:
+) -> Tuple[np.ndarray, Dict[str, int], Dict[str, int], np.ndarray]:
     """"""
     if entity_label_to_id is None or relation_label_to_id is None:
         entity_label_to_id, relation_label_to_id = create_mappings(triples)
@@ -25,11 +25,17 @@ def create_mapped_triples(
     subject_column = np.vectorize(entity_label_to_id.get)(triples[:, 0:1])
     relation_column = np.vectorize(relation_label_to_id.get)(triples[:, 1:2])
     object_column = np.vectorize(entity_label_to_id.get)(triples[:, 2:3])
-    triples_of_ids = np.concatenate([subject_column, relation_column, object_column], axis=1)
+    relation_type_column = np.array(
+        [[1] if rel=='<instance_of>' else [2] if rel=='<subclass_of>' else [0]
+         for rel in triples[:, 1:2]])
+    triples_of_ids = np.concatenate([subject_column, relation_column, object_column, relation_type_column], axis=1)
 
     triples_of_ids = np.array(triples_of_ids, dtype=np.long)
     # Note: Unique changes the order
-    return np.unique(ar=triples_of_ids, axis=0), entity_label_to_id, relation_label_to_id
+    triples_of_ids = np.unique(ar=triples_of_ids, axis=0)
+    types_of_triples = triples_of_ids[:, 3:4]
+    triples_of_ids = triples_of_ids[:, 0:3]
+    return triples_of_ids, entity_label_to_id, relation_label_to_id, types_of_triples
 
 
 def create_mappings(triples: np.ndarray) -> Tuple[Dict[str, int], Dict[str, int]]:
